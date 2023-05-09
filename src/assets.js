908c2f6fdc264eaa3107da7d4e7cab81f559b745
finowrin <90771885+finowrin@users.noreply.github.com>
2023-05-09 06:11:09 +0530

    Update assets.js

--- src/assets.js
+++ src/assets.js
@@ -2,3 +2,4 @@ export const logo = require("./assets/icons/logo.png");
 export const pacmanImg = require("./assets/pacman.png");
 export const breakoutImg = require("./assets/breakout.png");
 export const financebookImg = require("./assets/financebook.png");
+export const cantstopImg = require("./assets/cantstopImg.png");
