4b6bbed846fe8393b55c8e913b1812be6dd5bdbe
finowrin <90771885+finowrin@users.noreply.github.com>
2025-07-31 20:26:43 +0530

    fix aria controls

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Experience.css";
 
 const Experience = () => {
-
     return (
         <section className="container" id="experience">
             <h1 className="experience-heading">
@@ -10,16 +9,17 @@ const Experience = () => {
             </h1>
             <div className="row justify-content-center">
                 <div className="col-2">
-                    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                        <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">TD Bank</button>
-                        <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">CGI</button>
-                        <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Memorial University</button>
-                        <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Memorial University</button>
+                    <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <button className="nav-link active" id="v-pills-belairdirect-tab" data-bs-toggle="pill" data-bs-target="#v-pills-belairdirect" type="button" role="tab" aria-controls="v-pills-belairdirect" aria-selected="true">belairdirect</button>
+                        <button className="nav-link" id="v-pills-tdbank-tab" data-bs-toggle="pill" data-bs-target="#v-pills-tdbank" type="button" role="tab" aria-controls="v-pills-tdbank" aria-selected="false">TD Bank</button>
+                        <button className="nav-link" id="v-pills-cgi-tab" data-bs-toggle="pill" data-bs-target="#v-pills-cgi" type="button" role="tab" aria-controls="v-pills-cgi" aria-selected="false">CGI</button>
+                        <button className="nav-link" id="v-pills-mun-research-tab" data-bs-toggle="pill" data-bs-target="#v-pills-mun-research" type="button" role="tab" aria-controls="v-pills-mun-research" aria-selected="false">Memorial University</button>
+                        <button className="nav-link" id="v-pills-mun-support-tab" data-bs-toggle="pill" data-bs-target="#v-pills-mun-support" type="button" role="tab" aria-controls="v-pills-mun-support" aria-selected="false">Memorial University</button>
                     </div>
                 </div>
                 <div className="col-6">
-                    <div class="tab-content" id="v-pills-tabContent">
-                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                    <div className="tab-content" id="v-pills-tabContent">
+                        <div className="tab-pane fade show active" id="v-pills-belairdirect" role="tabpanel" aria-labelledby="v-pills-belairdirect-tab" tabIndex="0">
                             <h4>Licensed Insurance Agent @ belairdirect</h4>
                             <p>May. 2025 - Present</p>
                             <ul>
@@ -30,7 +30,7 @@ const Experience = () => {
                                 <li>Collaborate across departments to resolve issues, improve workflows, and support operational goals.</li>
                             </ul>
                         </div>
-                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                        <div className="tab-pane fade" id="v-pills-tdbank" role="tabpanel" aria-labelledby="v-pills-tdbank-tab" tabIndex="0">
                             <h4>Customer Experience Associate @ TD Bank</h4>
                             <p>March 2022 - April 2025</p>
                             <ul>
@@ -40,7 +40,7 @@ const Experience = () => {
                                 <li>Consistently exceeded quarterly goals, excelling in referrals and customer satisfaction surveys.</li>
                             </ul>
                         </div>
-                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+                        <div className="tab-pane fade" id="v-pills-cgi" role="tabpanel" aria-labelledby="v-pills-cgi-tab" tabIndex="0">
                             <h4>Digital Technology Transformation Consultant Co-op @ CGI</h4>
                             <p>Jan. 2024 - Aug. 2024</p>
                             <ul>
@@ -50,10 +50,8 @@ const Experience = () => {
                                 <li>Conducted daily health checks on cloud-hosted applications to ensure consistent performance.</li>
                                 <li>Led knowledge transfer sessions and documented processes in Confluence to enhance team collaboration.</li>
                             </ul>
-
                         </div>
-
-                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">
+                        <div className="tab-pane fade" id="v-pills-mun-research" role="tabpanel" aria-labelledby="v-pills-mun-research-tab" tabIndex="0">
                             <h4>Research Assistant - Department of Economics @ Memorial University of Newfoundland</h4>
                             <p>Aug. 2021 - May. 2022</p>
                             <ul>
@@ -62,7 +60,7 @@ const Experience = () => {
                                 <li>Wrote, proofread, and edited economic reports, abstracts, and briefs for publication.</li>
                             </ul>
                         </div>
-                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
+                        <div className="tab-pane fade" id="v-pills-mun-support" role="tabpanel" aria-labelledby="v-pills-mun-support-tab" tabIndex="0">
                             <h4>Student Services Support Assistant @ CITL, Memorial University</h4>
                             <p>Aug. 2022 – Dec. 2022</p>
                             <ul>
@@ -71,14 +69,9 @@ const Experience = () => {
                                 <li>Coordinated materials and distribution for exams and assignments at the distribution center.</li>
                             </ul>
                         </div>
-
                     </div>
-
-
                 </div>
-
             </div>
-
         </section>
     );
 };
